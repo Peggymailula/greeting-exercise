723233d8f92f4f3a2148ce3787fc9f02a1ee970a
Peggy <peggymailula@gmail.com>
2021-05-04 12:30:53 +0200

    adding colours to error messages

--- greet.js
+++ greet.js
@@ -1,15 +1,14 @@
 //adding querySelectors and refs
 
-var displayName = document.querySelector('#displayName');
-var getInput = document.querySelector('#inputBox');
-//var greetBtn = document.querySelector('#greetBtn');
-var resetBtn = document.querySelector('#resetBtn');
+var displayName = document.querySelector(".name");
+var getInput = document.querySelector(".input");
+var resetBtn = document.querySelector(".resetBtn");
 var displayCount = document.querySelector('#countNumber');
 
 var localLogic = {}
 
 if (localStorage['userMap']) {
-    //local storage can only store strings so convert to store object or arrays
+    
        localLogic = JSON.parse(localStorage['userMap']);
 }
 //an instance of the greet Factory
@@ -27,11 +26,11 @@ var getName = function () {
 }
 
 //function to clear the textbox 
-var clearBox = function () {
+var restart = function () {
     getInput.value = "";
 }
 
-//display the total counts of greetings
+//Used to set counter
 var setCounter = function () {
 
     displayCount.innerHTML = Object.keys(greet.getlocal()).length;
@@ -43,36 +42,48 @@ var submitForm =function(){
     displayName.classList.remove('red');
     var radioBtn = document.querySelector('input[name="radioLang"]:checked');
     var nameFromDom = getName().name;
-    console.log('type of name from DOM : '+typeof(parseFloat(nameFromDom)));
-    //ensure theres no empty name and there is a checked radio button
+   
     if (nameFromDom){
         if (radioBtn !== null) {
-            console.log(Object.keys(localLogic).length)
+          
             var langFromDom = greet.setLang(radioBtn.value);
             displayName.innerHTML = greet.greetNow(nameFromDom, langFromDom);
-            //console.log(greet.getlocal())
+            
             localStorage.setItem('userMap', JSON.stringify(greet.getlocal()));
             setCounter();
-            clearBox();
-        } else {
-            displayName.innerHTML = ('Please choose your language first');
+            restart();
+        } 
+        else {
+            t3 = setTimeout(function () { displayName.innerHTML ='ERROR! LANGUAGE  NOT SELECTED!' }, 0); 
             displayName.classList.add('red');
+            t4=setTimeout(function () { displayName.innerHTML = 'Please select language of choice.'; }, 3000); 
+            //localStorage.clear();
+            
         }
     } else {
-        displayName.innerHTML = 'Please type in name';
+        t3 = setTimeout(function () { displayName.innerHTML ='ERROR! NAME NOT ENTERED.' }, 0); 
+        //setTimeout(displayName.innerHTML = 'Please enter your name',5000) ;
         displayName.classList.add('red');
+         t2=setTimeout(function () { displayName.innerHTML = 'Please enter your name'; }, 3000); 
+          //displayName.classList.remove('green');
+        localStorage.clear();
+        //location.reload();
 
     }
         return false;
 };
 
-//Event listener for the reset button
 
-resetBtn.addEventListener('click', function run() {
-  
-    displayName.innerHTML = 'Congratulations! Application has been reset succesfully';
+
+resetBtn.addEventListener('click', function btn() {
+    displayName.classList.remove('red');
+    var t1 = setTimeout(function () { displayName.innerHTML = 'Congratulations! Application has been reset succesfully'; }, 0); 
+    var t2=setTimeout(function () { displayName.innerHTML = 'Please enter your name'; }, 3000); 
+    t1;
     displayName.classList.add('green');
+    t2;
+    //displayName.classList.remove('green');
     localStorage.clear();
     displayCount.innerHTML =0;
-   //location.reload();
+  // location.reload();
 });
